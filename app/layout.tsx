d82cc30d3a38a09521a15601e4471b0fd9f5e596
secret <146640389+ryoheixycz@users.noreply.github.com>
2024-10-08 15:13:23 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Facebook Picture Guard Manager",
+  description: "Manage your Facebook Picture Guard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
